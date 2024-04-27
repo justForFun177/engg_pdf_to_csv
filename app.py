ac895f0b92ef330a3889ff75800c0ee4806b35ff
justForFun177 <165027910+justForFun177@users.noreply.github.com>
2024-04-27 21:32:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,39 +3,38 @@ import pandas as pd
 import copy
 from io import StringIO
 import io
+import pdfplumber
 import processEnggResult
 import tabula
 import re
 import warnings
+
 warnings.filterwarnings("ignore", category=SyntaxWarning, message=r"invalid escape sequence.*")
 st.set_page_config(page_title="PDF to EXCEL")
 st.title("UPLOAD ENGINEERING RESULT PDF")
 
 try:
     pdf = st.file_uploader("UPLOAD A FILE")
-    
+
     if pdf:
-        pdf2 = copy.copy(pdf)
-        check_table = tabula.read_pdf(pdf, pages="3-10", multiple_tables=True)
-        name_pattern = r'Name:\s*([\w\s]+)'
-        prn_pattern = r'PRN:\s*(\d+)'
-        seat_pattern = r'Seat No:\s*(\d+)'
-        flag = 0
-        for i in range(len(check_table)):
-            pattern = check_table[i].columns[0]
-            if re.search(name_pattern, pattern) and re.search(prn_pattern, pattern) and re.search(seat_pattern, pattern):
-                flag = 1
-                break
-    
-        if flag==1:
-            data = processEnggResult.format2(pdf2)
+        tables = []
+
+        with pdfplumber.open(pdf) as a:
+            for i, page in enumerate(a.pages):
+                if i >= 2:
+                    break
+                table = page.extract_tables()
+                tables.extend(table)
+
+        if tables != []:
+            data = processEnggResult.format2(pdf)
             a = data.to_csv().encode('utf-8')
-            st.download_button("DOWNLOAD FILE",a, file_name="data.csv", mime="text/csv")
-    
-        else :
+            st.download_button("DOWNLOAD FILE", a, file_name="data.csv", mime="text/csv")
+
+        else:
             try:
                 data_list = processEnggResult.extract_data_from_pdf(pdf)
-    
+
                 html_head = """
                 <thead>
                     <tr>
@@ -147,14 +146,14 @@ try:
                     </tr>
                 </thead>
                 """
-    
+
                 all_data = ""
                 for student in data_list:
                     one_row = ""
                     one_row += f"<td>{student['Exam_Seat_No']}</td>"
                     one_row += f"<td>{student['PRN_No']}</td>"
                     one_row += f"<td>{student['Name']}</td>"
-    
+
                     for i in ["Sem3", "Sem4"]:
                         for data in student[i]:
                             # Check if the value is a dictionary before iterating over it
@@ -163,18 +162,17 @@ try:
                                     one_row += f"<td>{student[i][data][abb]}</td>"
                             else:
                                 one_row += f"<td>{student[i][data]}</td>"
-    
+
                     one_row += f"<td>{student['Status']}</td>"
                     one_row += f"<td>{student['Percentage']}</td>"
                     all_data += f"<tr>{one_row}</tr>"
-    
+
                 table = f"<table>{html_head}<tbody>{all_data}</tbody></table>"
                 result = pd.read_html(StringIO(table))
-    
-    
+
                 a = result[0].to_csv().encode('utf-8')
-                st.download_button("DOWNLOAD FILE",a, file_name="final.csv", mime="text/csv")
+                st.download_button("DOWNLOAD FILE", a, file_name="final.csv", mime="text/csv")
             except Exception as e:
                 st.error(e)
-except Exception:
-    st.error("PLEASE CHECK FILE FORMAT!!!")
+except Exception as e:
+    st.error("PLEASE CHECK FORMAT OF GIVEN FILE!!!")
